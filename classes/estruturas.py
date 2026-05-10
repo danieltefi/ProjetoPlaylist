e9173406f94b3e9569789e27d7da200e0f570513
Daniel <daniel.odontolegal@gmail.com>
2026-05-10 11:51:11 -0300

    Cria o arquivo estruturas.py
    Implementa a classe NodoLista e NodoFila.

---
+++ classes/estruturas.py
@@ -0,0 +1,16 @@
+from musica import Musica # importa sua classe Musica
+from typing import Optional
+
+class NodoLista: # nó para lista encadeada simples da biblioteca.
+    def __init__(self, musica: Musica):
+        self.musica = musica # o 'dado' do nó é um objeto Musica
+        self.proximo: Optional['NodoLista'] = None # referência para o próximo nó
+                                                   # optional é usado como sinalizador de "fim de linha"
+                                                   # se o próximo for None, sabe-se que chegou-se ao último elemento da biblioteca ou fila
+                                                   # ['NodoLista']: entre aspas pois na definição do atributo proximo a classe não terminou de ser criada ainda
+                                                   # ' ' avisa para esperar a definição terminar antes de validar o tipo
+
+class NodoFila: # nó encadeado usado internamente pela fila FIFO
+    def __init__(self, musica: Musica):
+        self.musica = musica # o 'dado' do nó
+        self.proximo: Optional['NodoFila'] = None # referência para o próximo
